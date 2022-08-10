5711f7ddc521c8ea63f8d3b29534550574936f11
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-08-10 10:27:23 +0500

    movie search

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,3 +1,34 @@
-export default function Main() {
-  return <div className="container content">All content</div>;
+import React from "react";
+import Movies from "../components/Movies";
+import Loader from "../components/Loader";
+import Search from "../components/Search";
+export default class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=47654af5&s=fast")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  searchMovies = (str) => {
+    fetch(`https://www.omdbapi.com/?apikey=47654af5&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
+
+  render() {
+    return (
+      <div className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {this.state.movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : (
+          <Loader />
+        )}
+      </div>
+    );
+  }
 }
