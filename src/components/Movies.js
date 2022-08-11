ea5d2c46dd8ffaa8cf17a644da8a4c61c283afc3
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-08-11 11:52:57 +0500

    404 event add

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,15 @@
 import Movie from "./Movie";
+import Img from "./404.jpg";
 export default function Movies(props) {
-  const { movies } = props;
+  const { movies = [] } = props;
 
   return (
     <div className="movies">
-      {movies.map((movie) => (
-        <Movie key={movie.imdbID} {...movie} />
-      ))}
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+      ) : (
+        <img className="img" src={Img} />
+      )}
     </div>
   );
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -5,22 +5,24 @@ import Search from "../components/Search";
 export default class Main extends React.Component {
   state = {
     movies: [],
+    loading: true,
   };
 
   componentDidMount() {
     fetch("https://www.omdbapi.com/?apikey=47654af5&s=fast")
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
   searchMovies = (str, type = "all") => {
+    this.setState({ loading: true });
     fetch(
       `https://www.omdbapi.com/?apikey=47654af5&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   };
 
   render() {
